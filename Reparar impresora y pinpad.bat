4208ff1f886dee40b89c6347331ce6295a4cfdc6
saymer <salvagym@gmail.com>
2017-04-05 19:56:52 +0200

    Batch Script to execute booth powershell scripts once
    Batch Script to execute booth powershell scripts once

---
+++ Reparar impresora y pinpad.bat
@@ -0,0 +1,2 @@
+Powershell.exe -executionpolicy remotesigned -File  \\srv-share\publico\scripts\impresoras_v2.ps1
+Powershell.exe -executionpolicy remotesigned -File  \\srv-share\publico\scripts\pinpad_v2.ps1
