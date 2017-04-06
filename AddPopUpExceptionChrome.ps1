93bfd80aae6270d6a72f5f5f3ada2758d3e91030
saymer <salvagym@gmail.com>
2017-04-06 20:22:23 +0200

    Script to add exception popup Chrome
    Script to add exception popup Chrome

---
+++ AddPopUpExceptionChrome.ps1
@@ -0,0 +1,23 @@
+﻿##################################################
+#
+#Created by: salvador madrid
+#Last update: 06/04/2017
+#
+#Description: Add website to allow exception popup
+#             on Chrome
+#	
+##################################################
+
+
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+
+$web = [Microsoft.VisualBasic.Interaction]::InputBox("Enter website name", "Add website to allow exception popup", "")
+#$web = 'https://www.google.es'
+
+
+$find = '"popups":{'
+$replace = '"popups":{"'+$web+',*":{"setting":1},'
+$path = 'Google\Chrome\User Data\Default\Preferences'
+
+(Get-Content $env:LOCALAPPDATA'\'$path).Replace($find,$replace) | Set-Content $env:LOCALAPPDATA'\'$path
+
